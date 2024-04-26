59816a2895460a84b467ee7f254954a9ff69d881
Muhammad Anwaar <anwarOfficial9@gmail.com>
2024-04-27 03:08:29 +0500

    Added Button in patientPractice

--- Components/Patient/PatientPractice.js
+++ Components/Patient/PatientPractice.js
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const PatientPractice = () => {
-  return (
+  const fetchData=async()=>{
+    Alert.alert("Clicked")
+  }
+    return (
     <View>
       <Text>PatientPractice</Text>
+
+
+      <Button title='Click Me' onPress={fetchData}></Button>
     </View>
   )
 }
